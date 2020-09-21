a19b56813774b136507bfc1184f17bc5659a8413
Mark Erickson <mje314@Marks-MacBook-Air.local>
2020-09-21 10:34:10 -0500

    added table data

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,12 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufo) => {
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
